e29452e2522d25e5a5d5fe17131929f1edfc6127
Luis Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2020-06-12 20:04:30 -0500

    feature: add angular material, navbar and toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,35 @@ import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 import { PabloComponent } from './pablo/pablo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './shared/components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailsComponent,
     HomeComponent,
-    PabloComponent
+    PabloComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
